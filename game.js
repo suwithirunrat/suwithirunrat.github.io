d28f376cfc536c75f3ee4fb2992923cfc7872a2a
suwithirunrat <64030318@KMITL.AC.TH>
2024-03-31 21:15:20 +0700

    upload

---
+++ game.js
@@ -0,0 +1,7 @@
+// Sample JavaScript code for a simple game
+// You can replace this with your actual game code
+window.onload = function() {
+    var gameContainer = document.getElementById('game-container');
+    gameContainer.innerHTML = '<h2>Simple JavaScript Game</h2>' +
+                              '<p>This is a simple game created with JavaScript.</p>';
+}
